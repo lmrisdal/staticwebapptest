e5ed5c8fb451408107640b204ce05ac7db32bc91
Lars Martin <larsrisdal@gmail.com>
2021-06-05 12:09:44 +0200

    test hardcoded

--- src/app/src/environments/environment.prod.ts
+++ src/app/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    clientId: '__clientid__',
-    authority: '__authority__',
-    redirectUrl: '__redirectUrl__',
+    clientId: 'c7c34960-82ad-400d-ba45-ddb6487aa319',
+    authority: 'https://login.microsoftonline.com/0b933fc9-3391-4302-9d86-85fe9bb5370e/',
+    redirectUrl: 'http://localhost:4200',
 };
