f9ec6f44ee78db5478957d58024bedaa6f721cc9
Lars Martin <larsrisdal@gmail.com>
2021-06-05 11:57:28 +0200

    test

--- src/app/src/environments/environment.ts
+++ src/app/src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
     production: false,
-    clientId: '__clientid__',
-    authority: '__authority__',
-    redirectUrl: '__redirectUrl__',
+    clientId: 'c7c34960-82ad-400d-ba45-ddb6487aa319',
+    authority: 'https://login.microsoftonline.com/0b933fc9-3391-4302-9d86-85fe9bb5370e/',
+    redirectUrl: 'http://localhost:4200',
 };
 
 /*
